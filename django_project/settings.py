bce6f65bc2f65d7b24ad02879275f15301880d52
Никита Кульпинов <ganggstand@gmail.com>
2022-07-30 13:48:36 +0300

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 # APPS
 # ------------------------------------------------------------------------------
